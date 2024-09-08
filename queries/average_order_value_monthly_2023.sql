2d6d99471719bb955480e6fbbca614eeb3d77d80
Adegoju5 <115003200+Adegoju5@users.noreply.github.com>
2024-09-08 23:20:53 +0200

    general adjustments

--- queries/average_order_value_monthly_2023.sql
+++ queries/average_order_value_monthly_2023.sql
@@ -2,7 +2,7 @@
 
 SELECT
     month,
-    ROUND(AVG(total_sales_amount), 2) AS average_order_value -- i rounded the average value up to two decimal place
+    ROUND(AVG(total_sales_amount), 2) AS average_order_value -- i rounded the average value up to two decimal places
 FROM transformed_sales_data
 GROUP BY month
 ORDER BY month;
